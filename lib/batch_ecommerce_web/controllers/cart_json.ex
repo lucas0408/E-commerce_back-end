1d874b4f1161519811ed9c09f0ee5f26157defb0
lucas0408 <113451742+lucas0408@users.noreply.github.com>
2024-11-23 20:52:07 -0300

    Update cart_json.ex

--- lib/batch_ecommerce_web/controllers/cart_json.ex
+++ lib/batch_ecommerce_web/controllers/cart_json.ex
@@ -2,13 +2,6 @@ defmodule BatchEcommerceWeb.CartJSON do
   alias BatchEcommerce.ShoppingCart.Cart
 
   @doc """
-  Renders a list of carts.
-  """
-  def index(%{carts: carts}) do
-    %{data: for(cart <- carts, do: data(cart))}
-  end
-
-  @doc """
   Renders a single cart.
   """
   def show(%{cart: cart}) do

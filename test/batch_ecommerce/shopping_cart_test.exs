dfef53cbcec25af2cd7ddf0bf44e3e8c96bc7b23
root <root@lucas0408.>
2025-04-19 18:34:08 -0300

    test: refactor shopping_cart_test.exs

--- test/batch_ecommerce/shopping_cart_test.exs
+++ test/batch_ecommerce/shopping_cart_test.exs
@@ -78,6 +78,7 @@ defmodule BatchEcommerce.ShoppingCartTest do
     end
 
     test "get_cart_product/1 get cart by id" do
+
         cart_product = insert(:cart_product)
 
         get_cart_product = ShoppingCart.get_cart_product(cart_product.id)
@@ -85,8 +86,69 @@ defmodule BatchEcommerce.ShoppingCartTest do
         assert cart_product.id == get_cart_product.id
         assert cart_product.price_when_carted == get_cart_product.price_when_carted
         assert cart_product.quantity == get_cart_product.quantity
-        assert cart_product.user_id = get_cart_product.user_id
+        assert cart_product.user_id == get_cart_product.user_id
         assert cart_product.product_id == get_cart_product.product_id
+    end
+
+    test "preload_product/1 loads product and categories data" do
+        cart_product = insert(:cart_product)
+
+        preload_cart_product = ShoppingCart.preload_product(cart_product)
+
+        assert %Ecto.Association.NotLoaded{} = cart_product.product
+
+        assert %BatchEcommerce.Catalog.Product{} = preload_cart_product.product
+
+        assert [%BatchEcommerce.Catalog.Category{}] = preload_cart_product.product.categories
+    end
+
+    test "update_cart_product/2 with valid data updates the cart_product" do
+        update_attrs = atom_keys_to_string(params_for(:cart_product))
+
+        cart_product = insert(:cart_product)
+
+        assert {:ok, %ShoppingCart.CartProduct{} = update_cart_product} = ShoppingCart.update_cart_product(cart_product, update_attrs)
+
+        assert update_cart_product.user_id == cart_product.user_id #need be equal 
+        assert update_cart_product.product_id != cart_product.product_id #product is updated
+    end
+
+    test "update_cart_product/2 with invalid data updates the cart_product" do
+        invalid_update_attrs = atom_keys_to_string(invalid_params_for(:cart_product, [:product_id]))
+
+        cart_product = insert(:cart_product)
+
+        assert {:error, :not_found} = ShoppingCart.update_cart_product(cart_product, invalid_update_attrs)
+
+        invalid_attrs = %{params_for(:cart_product) | quantity: -1}
+
+        invalid_cart_attrs = atom_keys_to_string(invalid_attrs)
+
+        assert {:error, %Ecto.Changeset{}} = ShoppingCart.update_cart_product(cart_product, invalid_cart_attrs)
+    end
+
+    test "total_price_cart_product/1 calculates total cart price" do
+        list_cart_product = insert_list(5, :cart_product)
+
+        total_price_function = ShoppingCart.total_price_cart_product(list_cart_product)
+
+        total_price_enum = Enum.reduce(list_cart_product, Decimal.new(0), fn cart_product, acc -> 
+            Decimal.add(acc, cart_product.price_when_carted)
+        end)
+
+        IO.inspect(total_price_enum)
+
+        assert total_price_function == total_price_enum
+    end
+
+    test "delete_cart_product/1 deletes the cart_product" do
+        cart_product = insert(:cart_product)
+
+        assert %CartProduct{} = ShoppingCart.get_cart_product(cart_product.id)
+
+        ShoppingCart.delete_cart_product(cart_product)
+
+        assert ShoppingCart.get_cart_product(cart_product.id) == {:error, :not_found}
     end
 
     def atom_keys_to_string(map) when is_map(map) do
@@ -96,9 +158,5 @@ defmodule BatchEcommerce.ShoppingCartTest do
         {k, v} -> {k, v}
         end)
     end
-
-
-
    end
-
 end

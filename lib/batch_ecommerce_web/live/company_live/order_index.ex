57a34c761ef30e8cbc10fc7a7555dcff352dbe1b
root <root@lucas0408.>
2025-06-20 01:31:09 -0300

    feat: adding title to table

--- lib/batch_ecommerce_web/live/company_live/order_index.ex
+++ lib/batch_ecommerce_web/live/company_live/order_index.ex
@@ -49,70 +49,70 @@ defmodule BatchEcommerceWeb.Live.CompanyLive.OrderIndex do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="max-w-7xl mx-auto px-4 py-8">
-      
     <.live_component module={BatchEcommerceWeb.Live.HeaderLive.HeaderDefault} user={@user} id="HeaderDefault"/>
-
-      <!-- Tabela de pedidos -->
-      <div class="mt-8 flow-root">
-        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <table class="min-w-full divide-y divide-gray-300">
-              <thead>
+    <div class="max-w-7xl mx-auto px-4 py-8">
+    
+    <!-- Tabela de pedidos -->
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Produto</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Cliente</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Quantidade</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total</th>
+                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Ação</th>
+                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                  <span class="sr-only">Ações</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white">
+              <%= for order_product <- @orders do %>
                 <tr>
-                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Produto</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Cliente</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Quantidade</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Total</th>
-                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span class="sr-only">Ações</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody class="divide-y divide-gray-200 bg-white">
-                <%= for order_product <- @orders do %>
-                  <tr>
-                    <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div class="flex items-center">
-                        <div class="h-11 w-11 flex-shrink-0">
-                          <img class="h-11 w-11 rounded-full" src={order_product.product.image_url} alt={order_product.product.name}>
-                        </div>
-                        <div class="ml-4">
-                          <div class="font-medium text-gray-900"><%= order_product.product.name %></div>
-                        </div>
+                  <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                    <div class="flex items-center">
+                      <div class="h-11 w-11 flex-shrink-0">
+                        <img class="h-11 w-11 rounded-full" src={order_product.product.image_url} alt={order_product.product.name}>
                       </div>
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div class="text-gray-900"><%= order_product.order.user.name %></div>
-                      <div class="mt-1 text-gray-500"><%= order_product.order.user.email %></div>
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <%= order_product.quantity %>
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                        <%= order_product.status %>
-                      </span>
-                    </td>
-                    <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <%= order_product.price |> Decimal.round(2) |> Decimal.to_string(:normal) %>
-                    </td>
-                    <td class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <.link 
-                        patch={~p"/companies/#{@company_id}/orders/#{order_product.id}"}
-                        class="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Ver mais
-                      </.link>
-                    </td>
-                  </tr>
-                <% end %>
-              </tbody>
-            </table>
-          </div>
+                      <div class="ml-4">
+                        <div class="font-medium text-gray-900"><%= order_product.product.name %></div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <div class="text-gray-900"><%= order_product.order.user.name %></div>
+                    <div class="mt-1 text-gray-500"><%= order_product.order.user.email %></div>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <%= order_product.quantity %>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      <%= order_product.status %>
+                    </span>
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                    R$ <%= order_product.price |> Decimal.round(2) |> Decimal.to_string(:normal) %>
+                  </td>
+                  <td class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <.link 
+                      patch={~p"/companies/#{@company_id}/orders/#{order_product.id}"}
+                      class="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Ver mais
+                    </.link>
+                  </td>
+                </tr>
+              <% end %>
+            </tbody>
+          </table>
         </div>
       </div>
+    </div>
 
       <!-- Paginação -->
       <nav class="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-0 mt-4">
